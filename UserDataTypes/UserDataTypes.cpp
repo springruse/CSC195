b329201419379cd3e142f2a3ed7f16a7869e5adf
springruse <152939330+springruse@users.noreply.github.com>
2025-04-28 21:00:46 -0600

    Added UserDataTypes

--- UserDataTypes/UserDataTypes.cpp
+++ UserDataTypes/UserDataTypes.cpp
@@ -6,5 +6,26 @@ using namespace std;
 int main()
 {
     int numOfEmployees;
-    std::cout << "Hello World!\n";
+    std::cout << "Number of Employees (Max 5): ";
+    cin >> numOfEmployees;
+
+    if (numOfEmployees > 5) {
+        numOfEmployees = 5;
+        
+    }
+
+    Employee employees[5];
+
+    for (int i = 0; i < numOfEmployees; i++) {
+        cout << "Enter Employee Details: " << (i+1) << endl;
+		employees[i].read();
+    }
+
+	std::cout << "Payroll Summary" << std::endl;
+	std::cout << "=================" << std::endl;
+	for (int i = 0; i < numOfEmployees; i++) {
+		employees[i].write();
+	}
+	return 0;
+
 }
