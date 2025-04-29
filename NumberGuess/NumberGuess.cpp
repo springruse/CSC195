1c89b366286468b86b7966a288eb8a4b16f3b8e4
springruse <152939330+springruse@users.noreply.github.com>
2025-04-29 09:20:13 -0600

    Added NumberGuess game

--- NumberGuess/NumberGuess.cpp
+++ NumberGuess/NumberGuess.cpp
@@ -10,8 +10,29 @@ int main()
     std::random_device diceroll;
     std::mt19937 generator(diceroll());
     std::uniform_int_distribution<int> roll1(1, 10);
+    int userAnswer = 0;
+    bool runProgram = true;
+    int targetNumber = roll1(generator);
 
-    std::cout << "Hello World!\n";
-}
+
+   while (runProgram) {
+        cout << "Pick a number between 1-10:" << endl;
+        cin >> userAnswer;
+
+        if (userAnswer > targetNumber) {
+            cout << "The number is too big! Try again." << endl;
+        }
+        else if (userAnswer < targetNumber) {
+            cout << "The number is too small! Try again." << endl;
+        }
+        else {
+            cout << "You've got the answer! It was: " << targetNumber << endl;
+            cout << "Thakns for playing!" << endl;
+            runProgram = false;
+        }
+   }
+
+   return 0;
+};
 
 
