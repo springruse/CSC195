5d96c68e8a26d3388df5a56c56483ee5be361781
springruse <152939330+springruse@users.noreply.github.com>
2025-04-30 00:12:36 -0600

    added Fraction's Assignment

--- Fractions/Fractions.cpp
+++ Fractions/Fractions.cpp
@@ -1,10 +1,32 @@
 
 
 #include <iostream>
-namespace std {}
+#include "fraction.h"
+
+using mathlib::Fraction;
 
 int main()
 {
-    // a
+	Fraction<int> f1(3, 4);
+	std::cout << "fraction 1: " << f1 << std::endl;
+	Fraction<int> f2(1, 2);
+	std::cout << "fraction 2: " << f2 << std::endl;
+
+	std::cout << "simplifying fraction 2" << std::endl;
+	std::cout << "simplified fraction 2: " << f2.simplify() << std::endl;
+
+	std::cout << f1 << " == " << f2 << " result: " << (f1 == f2) << std::endl;
+	std::cout << f1 << " > " << f2 << " result: " << (f1 > f2) << std::endl;
+
+	std::cout << f1 << " + " << f2 << " result: " << (f1 + f2) << std::endl;
+	std::cout << f1 << " * " << f2 << " result: " << (f1 * f2) << std::endl;
+	std::cout << f1 << " - " << f2 << " result: " << (f1 - f2) << std::endl;
+
+
+	Fraction<int> f3;
+	std::cout << "input fraction (numerator - denominator): \n";
+	std::cin >> f3;
+	std::cout << f3 << std::endl;
+	std::cout << f3.toFloat() << std::endl;
 }
 
--- Fractions/fraction.h
+++ Fractions/fraction.h
@@ -1,1 +1,129 @@
 #pragma once
+
+
+namespace mathlib {
+
+	template<typename T>
+
+	class Fraction {
+
+	public:
+
+		void checkForNegative() {
+			if (denominator < 0) {
+				numerator = -numerator;
+				denominator = -denominator;
+			}
+		}
+
+		Fraction() = default;
+
+		Fraction(T num, T deno) : numerator(num), denominator(deno) {
+
+			if (deno == 0) {
+				throw std::invalid_argument("Denominator cannot be zero");
+			}
+
+			checkForNegative();
+
+			numerator = num;
+			denominator = deno;
+
+		}
+		Fraction simplify() const {
+			T GCD = findGCD(abs(numerator), abs(denominator));
+			T simplifiedNum = numerator / GCD;
+			T simplifiedDeno = denominator / GCD;
+
+			if (simplifiedDeno < 0) {
+				simplifiedNum = -simplifiedNum;
+				simplifiedDeno = -simplifiedDeno;
+			}
+
+			return Fraction(simplifiedNum, simplifiedDeno);
+		}
+
+		float toFloat() const {
+			return static_cast<float>(numerator) / static_cast<float>(denominator);
+		}
+
+		T findGCD(T a, T b) const {
+			while (b != 0) {
+				T temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+		T getNumerator() const { return numerator; }
+		T getDenominator() const { return denominator; }
+
+		
+
+		// base math operators
+
+		Fraction operator + (const Fraction& other) const {
+			return Fraction{ numerator * other.denominator + other.numerator * denominator, denominator * other.denominator };
+		}
+
+		Fraction operator - (const Fraction& other) const {
+			return Fraction{ numerator * other.denominator - other.numerator * denominator, denominator * other.denominator };
+		}
+
+		Fraction operator * (const Fraction& other) const {
+			return Fraction{ numerator * other.numerator, denominator * other.denominator };
+		}
+
+		Fraction operator / (const Fraction& other) const {
+			return Fraction{ numerator * other.denominator, denominator * other.numerator };
+
+		}
+
+		// comparison operators
+
+		bool operator==(const Fraction& other) const {
+			Fraction a = this->simplify();
+			Fraction b = other.simplify();
+			return a.numerator == b.numerator && a.denominator == b.denominator;
+		}
+
+		bool operator !=(const Fraction& other) const {
+			return !(*this == other);
+		}
+
+		bool operator>(const Fraction& other) const {
+			return (numerator * other.denominator > other.numerator * denominator);
+		}
+
+		bool operator < (const Fraction& other) const {
+			return (numerator * other.denominator < other.numerator * denominator);
+		}
+
+		bool operator >= (const Fraction& other) const {
+			return (numerator * other.denominator >= other.numerator * denominator);
+		}
+
+		bool operator <= (const Fraction& other) const {
+			return *this < other || *this == other;
+		}
+
+		// stream operators
+
+		friend std::ostream& operator << (std::ostream& ostream, const Fraction& f) {
+			ostream << f.numerator << "/" << f.denominator;
+			return ostream;
+		}
+
+		friend std::istream& operator >> (std::istream& istream, Fraction& f) {
+			T num, den;
+			char slash;
+			istream >> num >> slash >> den;
+
+			f = Fraction(num, den);
+			return istream;
+		}
+	private:
+		T numerator = 0;
+		T denominator = 0;
+	};
+}
